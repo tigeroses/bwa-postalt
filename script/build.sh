103893d9c605651a72bfbaf2447e6fab65467bca
fxzhao <zhaofuxiang@genomics.cn>
2020-10-20 10:50:57 +0800

    fix compiling warning

--- script/build.sh
+++ script/build.sh
@@ -1,2 +1,3 @@
 cmake -Hstandalone -Bbuild/standalone
-cmake --build build/standalone
+cmake --build build/standalone 
+# cmake --install build/standalone --prefix /hwfssz5/ST_BIGDATA/USER/zhaofuxiang/release/Bwa-Postalt 
